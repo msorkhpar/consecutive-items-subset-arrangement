145b59dcfbc550a1f447f4938a37437c59d1e00b
Mo Sorkhpar <Sorkhpar@outlook.com>
2023-05-18 00:12:05 -0400

    Run multiple samples in concurrent
    - set up queue in multiprocessing package
    - add lock mechanism over the queue
    - modify docker environment to accept the configuration concurrent process number
    
    https://trello.com/c/htZaE2Am/23-run-multiple-samples-in-concurrent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
 import os
+from dataclasses import dataclass
+from multiprocessing import Lock, Process, Queue
 
 import pandas as pd
 from dotenv import load_dotenv
@@ -16,9 +18,57 @@ logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(funcName)s:%(me
                     level=int(os.getenv('LOG_LEVEL')))
 logger = logging.getLogger(__name__)
 
-if __name__ == '__main__':
+
+@dataclass
+class InputData:
+    nodes: int
+    min_pairs: int
+    max_pairs: int
+
+
+def process_task(input_data: InputData):
+    items, players, item_sets, edges = generate_random_edge_set(
+        input_data.nodes, input_data.min_pairs, input_data.max_pairs
+    )
+    G = construct_bipartite_graph(items, players, edges)
+    assign_random_values_to_item_set(G)
+    ceiling_big_item_values(G)
+    bigraph_id = persist_bigraph(G)
+    logger.info(
+        f"A sample with {len(items)} items, {len(players)} players, and {len(edges)} edges is created. "
+        f"DB id is [{bigraph_id}]"
+    )
+    lp_solver = Solver(G)
+    solution = lp_solver.solve()
+    if solution is None:
+        return
+    persist_fractional_solution(bigraph_id, solution.K, solution.variables, solution.running_time)
+    logger.info(f"Fractional K for sample[{bigraph_id}] has been found.")
+    if solution.K == 1.0:
+        lp_solver.change_to_integral()
+        solution = lp_solver.solve()
+        if solution is None:
+            return
+        persist_integral_solution(bigraph_id, solution.K, solution.variables, solution.running_time)
+        logger.info(f"Integral K for sample[{bigraph_id}] has been found.")
+    lp_solver.clear()
+
+
+def worker(q, lock):
+    while True:
+        with lock:
+            if not q.empty():
+                task = q.get()
+            else:
+                break
+        process_task(task)
+
+
+def main():
     load_dotenv()
     create_tables()
+    num_processes = int(os.environ.get("NUMBER_OF_PROCESSES"))
+    task_queue = Queue()
 
     plans = pd.read_csv('plan.csv', dtype={
         'nodes': int,
@@ -30,28 +80,18 @@ if __name__ == '__main__':
     for index, row in plans.iterrows():
         no_samples = row['samples']
         for i in range(no_samples):
-            items, players, item_sets, edges = generate_random_edge_set(
-                row['nodes'], row['min_pairs'], row['max_pairs']
-            )
-            G = construct_bipartite_graph(items, players, edges)
-            assign_random_values_to_item_set(G)
-            ceiling_big_item_values(G)
-            bigraph_id = persist_bigraph(G)
-            logger.info(
-                f"A sample with {len(items)} items, {len(players)} players, and {len(edges)} edges is created. "
-                f"DB id is [{bigraph_id}]"
-            )
-            lp_solver = Solver(G)
-            solution = lp_solver.solve()
-            if solution is None:
-                continue
-            persist_fractional_solution(bigraph_id, solution.K, solution.variables, solution.running_time)
-            logger.info(f"Fractional K for sample[{bigraph_id}] has been found.")
-            if solution.K == 1.0:
-                lp_solver.change_to_integral()
-                solution = lp_solver.solve()
-                if solution is None:
-                    continue
-                persist_integral_solution(bigraph_id, solution.K, solution.variables, solution.running_time)
-                logger.info(f"Integral K for sample[{bigraph_id}] has been found.")
-            lp_solver.clear()
+            task_queue.put(InputData(row['nodes'], row['min_pairs'], row['max_pairs']))
+
+    lock = Lock()
+    processes = []
+    for _ in range(num_processes):
+        p = Process(target=worker, args=(task_queue, lock))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+
+if __name__ == '__main__':
+    main()
